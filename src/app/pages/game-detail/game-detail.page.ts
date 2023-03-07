f212fc957cbc1022b8284f9fd48e66d35823d74e
Erlend Halsne Dahl <erlend.halsne.dahl@no.experis.com>
2023-03-07 10:06:31 +0100

    feat: created game-detail page component

---
+++ src/app/pages/game-detail/game-detail.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-detail',
+  templateUrl: './game-detail.page.html',
+  styleUrls: ['./game-detail.page.css']
+})
+export class GameDetailPage {
+
+}
