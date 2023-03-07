6125c2ef280b5f9accc94b46f903a81683be9b1c
Erlend Halsne Dahl <erlend.halsne.dahl@no.experis.com>
2023-03-07 15:53:44 +0100

    feat: created getGame function in game service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Game } from '../models/game.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  _game?: Game;
+
+  constructor(private readonly http: HttpClient) { }
+
+  public getGame(id: number): void{
+    this.http.get<Game>(`url/${id}`)
+    .subscribe({
+      next: (game: Game) => {
+        this._game = game;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    })
+  }
 }
