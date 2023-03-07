e9373e4e3764e42061dc99e50d7391a0e68fa9ac
Sondre Eftedal <sondre.eftedal@no.experis.com>
2023-03-07 13:06:35 +0000

    feat: Landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPage } from './pages/landing/landing.page';
+import { LandingPageListComponent } from './components/landing-page-list/landing-page-list.component';
 import { GameDetailPage } from './pages/game-detail/game-detail.page';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingPage,
+    LandingPageListComponent,
     GameDetailPage
   ],
   imports: [
