aa2e007c6f70f8906bdd314994da48bea5dd4588
Sondre Eftedal <sondre.eftedal@no.experis.com>
2023-03-08 09:55:15 +0000

    Sondre landingpage

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { PlayerService } from "src/app/services/player.service";
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.css']
 })
-export class LandingPage {
-
+export class LandingPage implements OnInit{
+  constructor(
+  ){}
+  ngOnInit(): void {
+    
+  }
 }
