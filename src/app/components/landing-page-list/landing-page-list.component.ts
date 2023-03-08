e54696c4094bf90f356e4b1b165c4cc97ac2e706
Erlend Halsne Dahl <erlend.halsne.dahl@no.experis.com>
2023-03-08 09:15:14 +0100

    feat: added goToGame() function to View button in landing-page-list

--- src/app/components/landing-page-list/landing-page-list.component.ts
+++ src/app/components/landing-page-list/landing-page-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { GameService } from "src/app/services/game.service";
 
 @Component({
   selector: 'app-landing-page-list',
@@ -7,4 +9,12 @@ import { Component } from "@angular/core";
 })
 export class LandingPageListComponent {
 
+  constructor(
+    private readonly gameService: GameService,
+    private readonly router: Router){}
+
+  goToGame(){
+    this.gameService.getGame(2)
+    this.router.navigateByUrl("/game-detail")
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Game } from '../models/game.model';
+
+const { apiUrl } = environment
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +15,11 @@ export class GameService {
   constructor(private readonly http: HttpClient) { }
 
   public getGame(id: number): void{
-    this.http.get<Game>(`url/${id}`)
+    this.http.get<Game>(`${apiUrl}/game`)
     .subscribe({
       next: (game: Game) => {
         this._game = game;
+        console.log(game)
       },
       error: (error: HttpErrorResponse) => {
         console.log(error.message);
