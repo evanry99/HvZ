0770038216c9777284fcd3f2de2c944b0645d7c1
Erlend Halsne Dahl <erlend.halsne.dahl@no.experis.com>
2023-03-09 10:16:52 +0100

    feat: updated map component to show play area

---
+++ src/app/models/map.model.ts
@@ -0,0 +1,6 @@
+export interface Map {
+    nw_lat: number;
+    nw_lng: number;
+    se_lat: number;
+    se_lng: number;
+}
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Game } from '../models/game.model';
+import { Map } from '../models/map.model';
+import { GameService } from './game.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  private _map?: Map;
+
+  get map(): Map{
+    if(!this._map){
+      throw new Error("No map")
+    }
+    return this._map;
+  }
+
+  constructor(private readonly gameService: GameService) { }
+
+  setMap(): void {
+    const game: Game = this.gameService.game
+    this._map = {
+      nw_lat : -43.11,//game.nw_lat,
+      nw_lng : 5.25,//game.nw_lng,
+      se_lat : game.se_lat,
+      se_lng : game.se_lng
+    }
+  }
+
 }
